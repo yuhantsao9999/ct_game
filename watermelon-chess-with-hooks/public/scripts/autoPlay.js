d9acbf974b878c3b6a981cbf4f1051c7ebf1dc61
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-04-07 10:31:39 +0800

    feat:取得正確的步數

--- watermelon-chess-with-hooks/public/scripts/autoPlay.js
+++ watermelon-chess-with-hooks/public/scripts/autoPlay.js
@@ -23,8 +23,8 @@ const speedIndex = {
 };
 
 const play = () => {
-    console.log('speed: ', document.getElementById('speed').innerHTML);
-    console.log('speed2: ', speedIndex[document.getElementById('speed').innerHTML]);
+    // console.log('step: ', document.getElementById('step').innerHTML);
+    // console.log('speed2: ', speedIndex[document.getElementById('speed').innerHTML]);
     // <option value="1000">-- 速度 --</option>
     // <option value="2000">0.25</option>
     // <option value="1500">0.5</option>
@@ -32,8 +32,8 @@ const play = () => {
     // <option value="500">1.5</option>
     // <option value="250">2</option>
     const step =
-        document.getElementById('step').value <= 100 && document.getElementById('step').value > 0
-            ? document.getElementById('step').value
+        document.getElementById('step').innerHTML <= 100 && document.getElementById('step').innerHTML > 0
+            ? document.getElementById('step').innerHTML
             : 100;
     if (step <= 100)
         for (let i = currentStep; i < step * 2; i++) {
