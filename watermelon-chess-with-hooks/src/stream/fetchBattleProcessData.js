bcadb4864cbe9f23b2d8748ef83f64f596ee05f2
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-05-27 19:22:46 +0800

    fix:debug

--- watermelon-chess-with-hooks/src/stream/fetchBattleProcessData.js
+++ watermelon-chess-with-hooks/src/stream/fetchBattleProcessData.js
@@ -1,9 +1,16 @@
 const fetchBattleProcessStream = async (pythonCodeData) => {
+    const { pythonCodeA, pythonCodeB } = pythonCodeData;
     console.log('do fetchBattleProcessStream');
     console.log('pythonCodeData', pythonCodeData);
+    console.log('pythonCodeA', pythonCodeA);
+    console.log('pythonCodeB', pythonCodeB);
+    const data = {
+        pythonCodeA,
+        pythonCodeB,
+    };
     if (pythonCodeData) {
         const formData = new FormData();
-        formData.append('pythonCodeData', JSON.stringify({ pythonCodeData }));
+        formData.append('pythonCodeData', JSON.stringify(data));
         fetch('http://140.122.164.194:5000/battle', {
             mode: 'cors',
             method: 'post',
