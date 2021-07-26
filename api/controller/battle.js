9bd28bc16fb7a111d980ba9c97925cb6c585c9d2
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-07-26 21:30:31 +0800

    debug

--- api/controller/battle.js
+++ api/controller/battle.js
@@ -17,11 +17,11 @@ const insertOneProcess = async (req) => {
 const findLatestProcess = async (req) => {
     const data = req.body;
     const cursor = await Battle.find(data);
-    const latestDoc = cursor.sort({ created_at: -1 });
-    console.log('latestDoc', latestDoc);
     while (cursor.hasNext()) {
         console.log('findLatestProcess', await cursor.next());
     }
+    const latestDoc = cursor.sort({ created_at: -1 });
+    console.log('latestDoc', latestDoc);
     // return latestDoc ? { error: false, data: latestDoc } : { error: true };
 };
 
