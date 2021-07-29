94510ff9179872f1cb97a411d946392dc8dc4b4a
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-07-29 16:34:35 +0800

    feat:upload error handling

--- api/public/js/upload.js
+++ api/public/js/upload.js
@@ -41,42 +41,68 @@ const uploadTeamFile = () => {
 
                 code = Blockly.Python.workspaceToCode(demoWorkspace);
                 // code =
-                //     'C0C1C2C3 = None\nC4C5 = None\nC6C7 = None\n\n\nfor C0C1C2C3 in board[yellow_player]:\n  if C0C1C2C3 != 0 or fun_Alive(C4C52(C0C1C2C3)) == False:\n    score = (score + 100)\n';
-                fetch('/api/insertPythonCode', {
+                //     "C0C1 = None\nC2C3 = None\nC4C5 = None\n\n\nfor C0C1 in board[yellow_player]:\n  if C0C1 != 0:\n    for C2C3 in Neighbor(C0C1):\n      if Exist_Chess(C2C3) == 0:\n        score = (score + 20)\n      elif Exist_Chess(C2C3) == 2:\n        score = (score + 15)\n      else:\n        score = (score - 20)\n"
+                standardCode =
+                    'C0C1 = None\nC2C3 = None\nC4C5 = None\n\n\nfor C0C1 in board[yellow_player]:\n  if C0C1 != 0:\n    for C2C3 in Neighbor(C0C1):\n      if Exist_Chess(C2C3) == 0:\n        score = (score + 20)\n      elif Exist_Chess(C2C3) == 2:\n        score = (score + 15)\n      else:\n        score = (score - 20)\n';
+
+                const pythonCodeData = {
+                    pythonCodeA: code,
+                    pythonCodeB: standardCode,
+                };
+                const formData = new FormData();
+                formData.append('pythonCodeData', JSON.stringify(pythonCodeData));
+
+                fetch('http://140.122.164.194:5000/battle', {
+                    mode: 'cors',
                     method: 'post',
-                    body: JSON.stringify({
-                        teamId,
-                        activityName,
-                        teamName,
-                        pythonCode: code,
-                    }),
-                    headers: {
-                        'content-type': 'application/json',
-                    },
+                    body: formData,
                 })
                     .then((response) => {
-                        return response;
+                        return response.json();
                     })
                     .then((response) => {
-                        console.log(response);
-                        fetch('/api/uploadFileName', {
-                            method: 'post',
-                            body: formData,
-                        })
-                            .then((response) => {
-                                console.log('uploadFileName response', response);
-                                return response;
+                        console.log('Error Proof battle response', response);
+                        if (response) {
+                            fetch('/api/insertPythonCode', {
+                                method: 'post',
+                                body: JSON.stringify({
+                                    teamId,
+                                    activityName,
+                                    teamName,
+                                    pythonCode: code,
+                                }),
+                                headers: {
+                                    'content-type': 'application/json',
+                                },
                             })
-                            .then((response) => {
-                                if (response.status == 200) {
-                                    console.log('Success:', response);
-                                    alert(`完成提交！`);
-                                }
-                            })
-                            .catch((error) => {
-                                alert(`上傳失敗請重新上傳`);
-                                console.error('Error:', error);
-                            });
+                                .then((response) => {
+                                    return response;
+                                })
+                                .then((response) => {
+                                    console.log(response);
+                                    fetch('/api/uploadFileName', {
+                                        method: 'post',
+                                        body: formData,
+                                    })
+                                        .then((response) => {
+                                            console.log('uploadFileName response', response);
+                                            return response;
+                                        })
+                                        .then((response) => {
+                                            if (response.status == 200) {
+                                                console.log('Success:', response);
+                                                alert(`完成提交！`);
+                                            }
+                                        })
+                                        .catch((error) => {
+                                            alert(`上傳失敗請重新上傳`);
+                                            console.error('Error:', error);
+                                        });
+                                })
+                                .catch((error) => console.error('Error:', error));
+                        } else {
+                            alert('上傳程式碼有誤，請檢查後再重新上傳');
+                        }
                     })
                     .catch((error) => console.error('Error:', error));
             });
