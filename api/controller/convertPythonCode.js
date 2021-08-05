566498163783a4b5bc3849e9a289a4285acc36f4
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-08-05 19:16:42 +0800

    fix:findLatestCode query

--- api/controller/convertPythonCode.js
+++ api/controller/convertPythonCode.js
@@ -14,7 +14,7 @@ const insertOneCode = async (req) => {
 const findLatestCode = async (req) => {
     const data = req.body;
     console.log('data', data);
-    const doc = await Code.find(data,{ $orderby: { teamName: teamName } };
+    const doc = await Code.findOne(data, { created_at: -1 });
     const latestDoc = await doc.next();
     return latestDoc ? { error: false, data: latestDoc } : { error: true };
 };
