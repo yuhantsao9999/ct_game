a04817e38f6015488a6cb2bfcc8f3085624eaa9f
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-09-29 13:55:57 +0800

    feat:remove log

--- api/controller/battle.js
+++ api/controller/battle.js
@@ -1,33 +1,8 @@
 const mysql = require('../module/db');
-function roughSizeOfObject(object) {
-    var objectList = [];
-    var stack = [object];
-    var bytes = 0;
 
-    while (stack.length) {
-        var value = stack.pop();
-
-        if (typeof value === 'boolean') {
-            bytes += 4;
-        } else if (typeof value === 'string') {
-            bytes += value.length * 2;
-        } else if (typeof value === 'number') {
-            bytes += 8;
-        } else if (typeof value === 'object' && objectList.indexOf(value) === -1) {
-            objectList.push(value);
-
-            for (var i in value) {
-                stack.push(value[i]);
-            }
-        }
-    }
-    return bytes;
-}
 const insertBattleProcess = async (teamData) => {
     try {
         const { activityName, playerA, playerB, process, winner, game } = teamData;
-        console.log('typeof process', typeof process);
-        console.log('roughSizeOfObject process', roughSizeOfObject(process));
         const sql = `INSERT INTO Battle (activityName, playerA, playerB, winner, process, game) VALUES ('${activityName}', '${playerA}', '${playerB}','${winner}','${process}','${game}') ON DUPLICATE KEY UPDATE process = '${process}', winner = '${winner}' `;
         const result = await mysql.query(sql).catch((err) => {
             console.log(err);
